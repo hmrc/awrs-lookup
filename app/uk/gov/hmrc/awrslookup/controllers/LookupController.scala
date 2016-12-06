22dfdc7ec2c35bcecb4eff485db11e6caad70d41
JoelTurner <joel.turner@digital.hmrc.gov.uk>
2016-12-06 09:20:26 +0000

    Removed comments

--- app/uk/gov/hmrc/awrslookup/controllers/LookupController.scala
+++ app/uk/gov/hmrc/awrslookup/controllers/LookupController.scala
@@ -21,49 +21,26 @@ import javax.inject.Inject
 import play.api.Environment
 import play.api.libs.json.Json
 import play.api.mvc._
-import uk.gov.hmrc.awrslookup.models.frontend.AwrsStatus.Pending
 import uk.gov.hmrc.awrslookup.models.frontend._
 import uk.gov.hmrc.awrslookup.services.EtmpLookupService
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class LookupController @Inject()(val environment: Environment) extends BaseController {
 
+  val referenceNotFoundString = "AWRS reference not found"
+
   val lookupService: EtmpLookupService = EtmpLookupService
 
-  //implicit def conv[T](data: T): Option[T] = Some(data)
-
-//  val testInfo = (id:String) => Info(s"testBusinessName$id", s"testTradingName$id", s"testFullName$id",
-//    Address(s"testline1$id", s"testline2$id", s"testline3$id", s"testline4$id", s"testPostCode$id", s"testCountry$id"))
-
   def lookup(awrsRef: String) = Action.async { implicit request =>
-//    val res:Option[SearchResult] = awrsRef match {
-//      case "XXAW00000123457" =>
-//        SearchResult(
-//          List(
-//            Business("XXAW00000123456", "1 April 2017", Pending, testInfo(" bus")),
-//            Group("XXAW00000123455", "1 April 2017", Pending, testInfo(" group"), List(testInfo(" member 1"), testInfo(" member 2")))
-//          ))
-//      case "XXAW00000123456" => SearchResult(List(Business("XXAW00000123456", "1 April 2017", Pending, testInfo(" bus"))))
-//      case "XXAW00000123455" => SearchResult(List(Group("XXAW00000123455", "1 April 2017", Pending, testInfo(" group"), List(testInfo(" member 1"), testInfo(" member 2")))))
-//      case "XXAW00000123454" => SearchResult(List())
-//      case _ => None
-//    }
-//    res match {
-//      case Some(r) =>
-//        val t = SearchResult.frontEndFormatter.writes(r)
-//        Future.successful(Ok(t))
-//      case _ =>
-//        Future.successful(NotFound("AWRS reference not found"))
-//    }
 
     lookupService.lookupApplication(awrsRef).map {
       lookupResponse =>
         lookupResponse.status match {
           case OK => val convertedJson = lookupResponse.json.as[SearchResult](SearchResult.etmpReader(environment = environment))
             Ok(Json.toJson(convertedJson))
+          case NOT_FOUND => NotFound(referenceNotFoundString)
           case _ => InternalServerError(lookupResponse.body)
         }
     }
