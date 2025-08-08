7ed6990ed4023157b466bcdc7157fe4bf8d3e1f6
Byasar3 <byasar3@users.noreply.github.com>
2025-08-08 17:13:06 +0100

    DL-17036: Fix vulnerabilities reported by MDTP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import play.sbt.PlayImport.*
 object AppDependencies {
 
   private val domainVersion            = "11.0.0"
-  private val bootstrapVersion         = "9.18.0"
+  private val bootstrapVersion         = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
